62cb000fca355cf7c3b138a026156e0a0009cf50
Adrien bouchet <adrien.bouchet.974@gmail.com>
2022-07-04 14:21:19 +0200

    Ajout de l'histoire.

--- public_html/index.php
+++ public_html/index.php
@@ -69,15 +69,84 @@
 
     $manager = new Manager($db);
     
-    $tank = new Warrior(21, 41, 31);
-    // $manager->create($tank);
-    $manager->update($tank, 10, 10, 10, 3);
+    $tank = new Warrior(20, 40, 30);
+    $manager->create($tank);
 
     $mage = new Wizard(30, 20, 10);
     $manager->create($mage);
 
     $archer = new Archer(25, 25, 15);
     $manager->create($archer);
+
+
+    echo 'Le guerrier attaque le sorcier !<br> Il subît '.($tank->getSword() - $mage->getDef()).' points de dégâts ! ';
+    if(($tank->getSword() - $mage->getDef()) < 0) {
+        echo 'Le guerrier s\'est blessé face à son adversaire possédant une plus grande défense que lui ! Le contre-coup lui a retiré'.abs($tank->getSword() - $mage->getDef()).' points de vie !';
+        $manager->update($tank, 20, ($tank->getLife() - (abs($tank->getSword() - $mage->getDef()))), 30, 1);
+    } else {
+        echo 'Il subît '.($tank->getSword() - $mage->getDef()).' points de dégâts !';
+    }
+    if($archer->getLife() > 0) {
+        $manager->update($mage, 30, ($mage->getLife()-($tank->getSword() - $mage->getDef())), 10);
+        echo'Le sorcier survit à l\'assaut du guerrier avec '.$db->execute("SELECT `life` FROM `archer`").' points de vie. Il riposte alors avec un sort qui lui infige 0 points de dégâts !';
+        if($tank->getLife() > 0) {
+            echo " Il survit !";
+        } else {
+            echo "Il meurt";
+        }
+    } else {
+        echo " Il meurt sur le coup !";
+    }
+    echo'<br><br>';
+    
+    // Continuer à partir d'ici.
+    // Le code ne fonctionne pas à partir d'ici.
+    
+    echo 'L\'archer tire une flèche sur le guerrier !<br> Il subît '.($archer->getBow() - $tank->getDef()).' points de dégâts ! ';
+    if() {
+        echo "Il survit avec ".." points de vie et décide de contre-attaquer, infligeant ".." points de dégâts à l'archer !";
+        echo '<br>';
+        if() {
+            echo "L'archer survit !";
+           
+        } else {
+            echo "L'archer ne survit pas à l'assaut. Il meurt sur le coup.";
+        }
+    } else {
+        echo " Il meurt sur le coup !";
+    }
+    echo '<br><br>';
+    if() {
+        echo "Le guerrier à gagner ! ";
+    }
+    if() {
+        echo "L'archer à gagner ! ";
+    }
+    if() {
+        echo "Le sorcier à gagner !";
+    }
+    echo '<br><br>';
+    
+    if(){
+        
+        echo 'A NEW CHALLENGER IS APPROACHING !!!<br>';
+        
+        echo 'Les combattants ne peuvent pas gagner s\'ils n\'unissent pas leurs forces !<br><br>';
+        if() {
+            echo "Nos combattants ont décidés de s'allier dans cette ultime bataille. Le sorcier utilise son sort de soin le plus puissant qu'il possède, ce qui permet à nos combattants de récupérer ".$heal." points de vie.";
+            echo '<br><br>';
+            echo "Nos héros possèdent respectivement "..", "..", ".;
+            if() {
+                echo '<br>';
+                echo "n'ayant pas assez d'endurance pour continuer, nos combattants s'éffondre sur le champ de bataille. La créature dévorant nos champions par la même occasion ...";
+            } else {
+                echo '<br>';
+                echo 'Grâce au sorcier, nos combattants ont pu abattre l\'ennemie et sauver le monde !<br><p style="text-align: center;">VICTOIRE</p>';
+            }
+        } else {
+            echo 'Nos combattants n\'ont pas sû trouver un terrain d`\'entente. La créature en a profité pour tous les tuer ...<br><p style="text-align: center;">GAME OVER</p>';
+        }
+    }
 
     
     ?>
